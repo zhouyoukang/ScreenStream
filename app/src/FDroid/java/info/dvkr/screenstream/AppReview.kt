c680baaa85bf5d89c1bf6281a6af77342608a4c5
Zhouyoukang <Zhouyoukang@users.noreply.github.com>
2026-02-15 03:23:49 +0800

    refactor: remove old app/ monolith structure

--- app/src/FDroid/java/info/dvkr/screenstream/AppReview.kt
+++
@@ -1,8 +0,0 @@
-package info.dvkr.screenstream
-
-import android.app.Activity
-
-public object AppReview {
-    public suspend fun showReviewUi(activity: Activity?) {
-    }
-}
--- app/src/main/java/info/dvkr/screenstream/ui/theme/Type.kt
+++
@@ -1,5 +0,0 @@
-package info.dvkr.screenstream.ui.theme
-
-import androidx.compose.material3.Typography
-
-internal val AppTypography = Typography()
